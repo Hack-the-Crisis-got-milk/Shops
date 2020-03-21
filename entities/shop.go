20354266206bea9fa2042654c3224ba04be9a0d4
Kristijonas Zalys <zalyk@amazon.com>
2020-03-21 12:33:54 +0100

    Implement shop entity

---
+++ entities/shop.go
@@ -0,0 +1,15 @@
+package entities
+
+type Location struct {
+	Lat  float64 `json:"lat"`
+	Long float64 `json:"long"`
+}
+
+type Shop struct {
+	ID      string   `json:"id"`
+	Name    string   `json:"name"`
+	Loc     Location `json:"loc"`
+	Address string   `json:"address"`
+	OpenNow string   `json:"open_now"`
+	Photo   string   `json:"photo"`
+}
