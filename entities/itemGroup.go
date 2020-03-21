5ed41ed8c700048859d63f918f3841fd8449f81c
Saulius-Saulys <sauliusaulys@gmail.com>
2020-03-22 01:36:16 +0200

    Update itemGroup.go

--- entities/itemGroup.go
+++ entities/itemGroup.go
@@ -4,5 +4,5 @@ type ItemGroup struct {
 	ID   string     `json:"id"          bson:"id"         yaml:"id"`
 	Name string     `json:"name"        bson:"name"       validate:"required" yaml:"name"`
 	Icon string     `json:"icon"        bson:"icon"       yaml:"icon"`
-	ShopType string `json:"shopt_type"  bson:"shopt_type" yaml:"shopt_type"`
+	ShopType string `json:"shop_type"   bson:"shop_type"  yaml:"shop_type"`
 }
