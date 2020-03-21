d969c5ca9cf8d12ca404b0e39372f5be616c96d9
Saulius-Saulys <sauliusaulys@gmail.com>
2020-03-22 01:11:24 +0200

    Update itemGroup.go

--- entities/itemGroup.go
+++ entities/itemGroup.go
@@ -3,4 +3,5 @@ package entities
 type ItemGroup struct {
 	ID   string `json:"id" bson:"id" yaml:"id"`
 	Name string `json:"name"  bson:"name" validate:"required" yaml:"name"`
+	Photo string `json:"photo"  bson:"photo" yaml:"photo"`
 }
